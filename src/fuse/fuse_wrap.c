a26008661d451f97dbbb44f1bf7f3921d6fec560
Frank Laub <frank.laub@gmail.com>
2014-09-23 16:54:11 -0700

    Fix Linux build

--- src/fuse/fuse_wrap.c
+++ src/fuse/fuse_wrap.c
@@ -11,9 +11,16 @@ const static uint64_t block_size = 1024;
 static void* bm = NULL;
 static uint64_t file_size = 0;
 static uid_t uid = 0;
+
+#ifdef __APPLE__
 static struct timespec create_time;
 static struct timespec modify_time;
 static struct timespec access_time;
+#else
+static time_t create_time;
+static time_t modify_time;
+static time_t access_time;
+#endif
 
 extern void* open_block_map(const char* dir, uint32_t blocks, const char* key);
 extern void close_block_map(void* bm);
@@ -21,27 +28,36 @@ extern int64_t size_block_map(void* bm);
 extern int read_block_map(void* bm, uint32_t block, char* buf);
 extern int write_block_map(void* bm, uint32_t block, const char* buf);
 
-static int safedisk_getattr(const char *path, struct stat *stbuf)
+static 
+int safedisk_getattr(const char* path, struct stat* st)
 {
-	memset(stbuf, 0, sizeof(struct stat));
+	memset(st, 0, sizeof(*st));
 
 	if (strcmp(path, "/") == 0) { 
 		// The root directory of our file system
-		stbuf->st_mode = S_IFDIR | 0755;
-		stbuf->st_nlink = 3;
-	} else if (strcmp(path, "/data") == 0) { 
+		st->st_mode = S_IFDIR | 0755;
+		st->st_nlink = 3;
+	} 
+	else if (strcmp(path, "/data") == 0) { 
 		// The data file
-		stbuf->st_mode = S_IFREG | 0644;
-		stbuf->st_nlink = 1;
-		stbuf->st_size = file_size;
-		stbuf->st_uid = uid;
-		stbuf->st_atimespec = access_time;
-		stbuf->st_mtimespec = modify_time;
-		stbuf->st_ctimespec = create_time;
-		stbuf->st_birthtimespec = create_time;
-		stbuf->st_blocks = file_size / 512;
-		stbuf->st_blksize = block_size;
-	} else { 
+		st->st_mode = S_IFREG | 0644;
+		st->st_nlink = 1;
+		st->st_size = file_size;
+		st->st_uid = uid;
+#ifdef __APPLE__
+		st->st_atimespec = access_time;
+		st->st_mtimespec = modify_time;
+		st->st_ctimespec = create_time;
+		st->st_birthtimespec = create_time;
+#else
+		st->st_atime = access_time;
+		st->st_mtime = modify_time;
+		st->st_ctime = create_time;
+#endif
+		st->st_blocks = file_size / 512;
+		st->st_blksize = block_size;
+	} 
+	else { 
 		// Reject everything else
 		return -ENOENT;
 	}
@@ -49,7 +65,8 @@ static int safedisk_getattr(const char *path, struct stat *stbuf)
 	return 0;
 }
 
-static int safedisk_open(const char *path, struct fuse_file_info *fi)
+static 
+int safedisk_open(const char* path, struct fuse_file_info* fi)
 {
 	if (strcmp(path, "/data") != 0) {
 		// We only recognize one file
@@ -59,8 +76,13 @@ static int safedisk_open(const char *path, struct fuse_file_info *fi)
 	return 0;
 }
 
-static int safedisk_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-			  off_t offset, struct fuse_file_info *fi)
+static 
+int safedisk_readdir(
+	const char*path, 
+	void *buf, 
+	fuse_fill_dir_t filler,
+	off_t offset, 
+	struct fuse_file_info* fi)
 {
 	if (strcmp(path, "/") != 0) {
 		// We only recognize the root directory
@@ -74,8 +96,13 @@ static int safedisk_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 	return 0;
 }
 
-static int safedisk_read(const char *path, char *buf, size_t size, off_t offset,
-		   struct fuse_file_info *fi)
+static 
+int safedisk_read(
+	const char* path, 
+	char* buf, 
+	size_t size, 
+	off_t offset,
+	struct fuse_file_info* fi)
 {
 	if (strcmp(path, "/data") != 0) {
 		// We can only be reading from the data file
@@ -97,13 +124,16 @@ static int safedisk_read(const char *path, char *buf, size_t size, off_t offset,
 		return -EIO;
 	}
 	// Update time
+#ifdef __APPLE__
 	access_time.tv_sec = time(0);
-
+#else
+	access_time = time(0);
+#endif
 	// Handle everything in blocks	
 	size /= block_size;
 	offset /= block_size;
 	uint32_t block;
-	for(block = 0; block < size; block++) {
+	for (block = 0; block < size; block++) {
 		if (!read_block_map(bm, offset + block, ((char*) buf) + block * block_size)) {
 			return -EIO;
 		}
@@ -112,8 +142,13 @@ static int safedisk_read(const char *path, char *buf, size_t size, off_t offset,
 	return size*block_size;
 }
 
-static int safedisk_write(const char *path, const char *buf, size_t size, off_t offset,
-		   struct fuse_file_info *fi)
+static 
+int safedisk_write(
+	const char* path, 
+	const char* buf, 
+	size_t size, 
+	off_t offset,
+	struct fuse_file_info* fi)
 {
 	if (strcmp(path, "/data") != 0) {
 		// We can only be writing to the data file
@@ -135,14 +170,19 @@ static int safedisk_write(const char *path, const char *buf, size_t size, off_t 
 		return -EIO;
 	}
 	// Update time
+#ifdef __APPLE__
 	modify_time.tv_sec = time(0);
 	access_time.tv_sec = time(0);
+#else
+	modify_time = time(0);
+	access_time = time(0);
+#endif
 
 	// Handle everything in blocks	
 	size /= block_size;
 	offset /= block_size;
 	uint32_t block;
-	for(block = 0; block < size; block++) {
+	for (block = 0; block < size; block++) {
 		if (!write_block_map(bm, offset + block, ((const char*) buf) + block * block_size)) {
 			return -EIO;
 		}
@@ -160,7 +200,7 @@ static struct fuse_operations safedisk_filesystem_operations = {
 };
 
 // TODO: Less lame option parsing 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
 	// Validate two extra arguments are there
 	if (argc < 4) {
@@ -191,15 +231,20 @@ int main(int argc, char **argv)
 	// Set global variables
 	file_size = size_block_map(bm);
 	uid = st_buf.st_uid;
+#ifdef __APPLE__
 	memset(&create_time, 0, sizeof(struct timespec));
 	memset(&modify_time, 0, sizeof(struct timespec));
 	memset(&access_time, 0, sizeof(struct timespec));
 	create_time.tv_sec = time(0);
 	modify_time.tv_sec = time(0);
 	access_time.tv_sec = time(0);
+#else
+	create_time = time(0);
+	modify_time = time(0);
+	access_time = time(0);
+#endif
 	// Kick off main
-	int r = fuse_main(argc - 2, argv, &safedisk_filesystem_operations, NULL);
+	int retcode = fuse_main(argc - 2, argv, &safedisk_filesystem_operations, NULL);
 	close_block_map(bm);
-	return r;
+	return retcode;
 }
-
--- src/test/test_block_map.cpp
+++ src/test/test_block_map.cpp
@@ -1,7 +1,8 @@
 #include "block_map.h"
 #include <assert.h>
 
-class check_block_map {
+class check_block_map 
+{
 public:
 	check_block_map(uint32_t size, const string& dir)
 		: m_size(size)
@@ -12,7 +13,8 @@ public:
 		assert(m_block_map->open(dir));
 	}
 
-	void write(uint32_t logical) {
+	void write(uint32_t logical) 
+	{
 		//printf("Writing to %d\n", (int) logical);
 		slice_t data(s_bytes_per_block);
 		for (size_t i = 0; i < s_bytes_per_block; i++) {
@@ -23,7 +25,8 @@ public:
 		m_check[logical] = data;
 	}
 
-	void read(uint32_t logical) {
+	void read(uint32_t logical) 
+	{
 		//printf("Reading from %d\n", (int) logical);
 		auto it = m_check.find(logical);
 		rslice_t check;
@@ -31,7 +34,8 @@ public:
 			slice_t empty(s_bytes_per_block);
 			memset(empty.buf(), 0, s_bytes_per_block);
 			check = empty;
-		} else {
+		} 
+		else {
 			check = it->second;
 		}
 		rslice_t proper;
@@ -55,8 +59,10 @@ private:
 
 void test_block_map()
 {
-	system("rm -rf /tmp/test_block_map");
-	system("mkdir /tmp/test_block_map");
+	int retcode = system("rm -rf /tmp/test_block_map");
+	assert(!retcode);
+	retcode = system("mkdir /tmp/test_block_map");
+	assert(!retcode);
 	size_t size = 333;
 	check_block_map cbm(size, "/tmp/test_block_map");
 	for (size_t i = 0; i < 100000; i++) {
