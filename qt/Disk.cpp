85b1d439bf6baefe4292dea7914f833cd3296554
Frank Laub <frank.laub@gmail.com>
2014-10-06 14:14:45 -0700

    Add copyright notices

--- qt/Disk.cpp
+++ qt/Disk.cpp
@@ -1,3 +1,20 @@
+/*  osx-nbd
+ *  Copyright (C) 2014 Frank Laub
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "Disk.h"
 
 #include <QDir>
@@ -93,7 +110,7 @@ void Disk::toggleMount()
 void Disk::displaySettings()
 {
 	m_parent->raise();
-	QMessageBox::information(m_parent, "SafeDisk", "Restore Disk");
+	QMessageBox::information(m_parent, "SafeDisk", "Settings");
 }
 
 void Disk::revealFolder()
